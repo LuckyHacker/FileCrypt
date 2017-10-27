0b446cbbd2e13799d372384ede76f085e7ddec83
root <k1977@student.jamk.fi>
2017-10-27 10:26:49 -0400

    Modified and added features in UI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 import algorithms
 
 class MainWindow:
@@ -11,10 +12,62 @@ class MainWindow:
 
     def init_ui(self):
         self.root = Tk()
-        self.root.geometry('400x600+350+70')
-        self.button = Button(self.root, text = 'Press me', command = self.Call)
-        self.button.pack()
+        self.root.title("ACB crypting machine")
+        self.root.geometry("600x400+350+70")
+        #self.button = Button(self.root, text = 'Press me', command = self.Call)
+        #self.button.pack()
+
+        #DROPDOWN MENU
+        dropdownHeader = Label(self.root)
+        dropdownHeader.config(text="Please choose a cipher:")
+        dropdownHeader.pack()
+
+        #JUST A TEXT BOX REMINDER
+        #text = Text(self.root, height=1, width=23)
+        #text.pack()
+        #text.insert(END, "Please choose a cipher:")
+        #text.config(state=DISABLED)
+
+        Options = ["AES", "Crypt", "Blowfish"]
+        variable = StringVar(self.root)
+        variable.set(Options[0])
+        w = OptionMenu(*(self.root, variable) + tuple(Options))
+        w.pack(ipadx=10, pady=10)
+
+        #CHECKBOXES
+        checkboxHeader = Label(self.root)
+        checkboxHeader.config(text="Do you want to decrypt, encrypt or both?")
+        checkboxHeader.pack()
+        enc = IntVar(self.root)
+        radioEnc = Radiobutton(self.root, text="Encrypt", variable=enc)
+        radioEnc.pack()
+        dec = IntVar(self.root)
+        radioDec = Radiobutton(self.root, text="Decrypt", variable=dec)
+        radioDec.deselect()
+        radioDec.pack()
+
+        #BROWSE BUTTON
+        browseHeader = Label(self.root)
+        browseHeader.config(text="Choose file to encrypt/decrypt:")
+        browseHeader.pack(pady=10)
+        self.browsebutton = Button(self.root, text="Browse", command=self.Browsefunc)
+        self.browsebutton.pack(ipadx=10)
+
         self.root.mainloop()
+
+    def Browsefunc(self):
+        pathHeader = Label(self.root)
+        pathHeader.config(text="Chosen file:")
+        pathHeader.pack()
+        #FILE PATH
+        filename = filedialog.askopenfilename()
+        self.pathlabel = Label(self.root)
+        self.pathlabel.config(text=filename)
+        self.pathlabel.pack()
+
+        #START BUTTON
+        self.startbutton = Button(self.root, text="Start!")
+        self.startbutton.pack(ipadx=15, pady=10)
 
     def Call(self):
         lab = Label(self.root, text = 'You pressed\nthe button')
